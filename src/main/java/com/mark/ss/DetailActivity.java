3cc63542e65b3686b9832a7660ba614627d4220a
xuqian <xuqian@jiudaifu.com>
2017-06-16 20:01:50 +0800

    change app icon

--- src/main/java/com/mark/ss/DetailActivity.java
+++ src/main/java/com/mark/ss/DetailActivity.java
@@ -3,18 +3,26 @@ package com.mark.ss;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.TextView;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         TextView text = (TextView) findViewById(R.id.text_detail);
+        View layout = findViewById(R.id.layout_detail);
+        layout.setOnClickListener(this);
         String content = getIntent().getStringExtra("content");
         if (!TextUtils.isEmpty(content)){
             text.setText(content);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        finish();
     }
 }
